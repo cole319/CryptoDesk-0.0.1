402d77e93632e3f75d001613aea2f6c037db1834
Suryarghya <suryar1332001@gmail.com>
2023-02-07 00:53:35 +0530

    modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
+// import React from "react";
+// import logo from "./logo.svg";
+// import { Counter } from "./features/counter/Counter";
 import "./App.css";
 import { LoginForm } from "./components/auth/login-form/login-form.component";
 
